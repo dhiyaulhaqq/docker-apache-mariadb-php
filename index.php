f9fb4c002fa0f88e51655c6ff548e15b8e3a200d
dhiyaulhaqq <46219120+dhiyaulhaqq@users.noreply.github.com>
2020-04-18 19:31:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ $user = 'root';
 $pass = 'example';
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=mysql', $user, $pass);
-    }
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
